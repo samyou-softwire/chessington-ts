55cb80d72ec6ff35dd252dc35783f52ab4bcbe97
Sammy Young <Samuel.Young@softwire.com>
2023-10-18 10:39:16 +0100

    Make pawns do pawn things

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,10 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const square = board.findPiece(this);
+
+        if (this.player === Player.WHITE)
+            return [Square.at(square.row+1, square.col)];
+        else return [Square.at(square.row-1, square.col)];
     }
 }
