9ef57ba7459f9e60cb33786e0a8f91aeb1e9c7c3
samyou-softwire <samuel.young@softwire.com>
2023-10-18 11:02:03 +0100

    bishop

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,21 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const moves: Square[] = [];
+        const square = board.findPiece(this);
+
+        for (let i = 1; i < 7; i++) {
+            tryAdd(moves, Square.at(square.row + i, square.col + i));
+            tryAdd(moves, Square.at(square.row - i, square.col + i));
+            tryAdd(moves, Square.at(square.row - i, square.col - i));
+            tryAdd(moves, Square.at(square.row + i, square.col - i));
+        }
+
+        return moves;
     }
+}
+
+function tryAdd(moves: Square[], square: Square) {
+    if (square.row >= 0 && square.col >= 0 && square.row <= 7 && square.col <= 7)
+        moves.push(square);
 }
