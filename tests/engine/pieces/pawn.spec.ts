51b77ccbde16a42f698af3bdeabf6d3529c1663f
samyou-softwire <samuel.young@softwire.com>
2023-10-19 10:06:44 +0100

    add tests for en passant

--- tests/engine/pieces/pawn.spec.ts
+++ tests/engine/pieces/pawn.spec.ts
@@ -83,6 +83,60 @@ describe('Pawn', () => {
 
             moves.should.not.deep.include(Square.at(5, 3));
         });
+
+        it('can take en passant', () => {
+            board = new Board(Player.BLACK);
+
+            const pawn = new Pawn(Player.WHITE);
+            const opposingPawn = new Pawn(Player.WHITE);
+
+            board.setPiece(Square.at(4, 5), pawn);
+            board.setPiece(Square.at(6, 6), opposingPawn);
+
+            opposingPawn.moveTo(board, Square.at(4, 6)); // move forwards 2 squares
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.deep.include(Square.at(5, 6));
+        });
+
+        it('cant take en passant after a move', () => {
+            board = new Board(Player.BLACK);
+
+            const pawn = new Pawn(Player.WHITE);
+            const opposingPawn = new Pawn(Player.WHITE);
+            const king = new King(Player.WHITE);
+            const opposingKing = new King(Player.BLACK); // need some extra pieces to waste moves
+
+            board.setPiece(Square.at(4, 5), pawn);
+            board.setPiece(Square.at(6, 6), opposingPawn);
+            board.setPiece(Square.at(0, 0), king);
+            board.setPiece(Square.at(7, 0), opposingKing);
+
+            opposingPawn.moveTo(board, Square.at(4, 6)); // move forwards 2 squares
+            king.moveTo(board, Square.at(1, 0));
+            opposingKing.moveTo(board, Square.at(6, 0));
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(5, 6));
+        });
+
+        it('cant take en passant if no double', () => {
+            board = new Board(Player.BLACK);
+
+            const pawn = new Pawn(Player.WHITE);
+            const opposingPawn = new Pawn(Player.WHITE);
+
+            board.setPiece(Square.at(4, 5), pawn);
+            board.setPiece(Square.at(5, 6), opposingPawn);
+
+            opposingPawn.moveTo(board, Square.at(4, 6)); // move forwards 1 square
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(5, 6));
+        });
     });
 
     describe('black pawns', () => {
@@ -160,6 +214,60 @@ describe('Pawn', () => {
             const moves = pawn.getAvailableMoves(board);
 
             moves.should.not.deep.include(Square.at(3, 3));
+        });
+
+        it('can take en passant', () => {
+            board = new Board(Player.WHITE);
+
+            const pawn = new Pawn(Player.BLACK);
+            const opposingPawn = new Pawn(Player.WHITE);
+
+            board.setPiece(Square.at(3, 2), pawn);
+            board.setPiece(Square.at(1, 1), opposingPawn);
+
+            opposingPawn.moveTo(board, Square.at(3, 1)); // move forwards 2 squares
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.deep.include(Square.at(2, 1));
+        });
+
+        it('cant take en passant after a move', () => {
+            board = new Board(Player.WHITE);
+
+            const pawn = new Pawn(Player.BLACK);
+            const opposingPawn = new Pawn(Player.WHITE);
+            const king = new King(Player.BLACK);
+            const opposingKing = new King(Player.WHITE); // need some extra pieces to waste moves
+
+            board.setPiece(Square.at(3, 2), pawn);
+            board.setPiece(Square.at(1, 1), opposingPawn);
+            board.setPiece(Square.at(7, 7), king);
+            board.setPiece(Square.at(0, 7), opposingKing);
+
+            opposingPawn.moveTo(board, Square.at(3, 1)); // move forwards 2 squares
+            king.moveTo(board, Square.at(6, 7));
+            opposingKing.moveTo(board, Square.at(1, 7));
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(2, 1));
+        });
+
+        it('cant take en passant if no double', () => {
+            board = new Board(Player.WHITE);
+
+            const pawn = new Pawn(Player.BLACK);
+            const opposingPawn = new Pawn(Player.WHITE);
+
+            board.setPiece(Square.at(3, 2), pawn);
+            board.setPiece(Square.at(2, 1), opposingPawn);
+
+            opposingPawn.moveTo(board, Square.at(3, 1)); // move forwards 1 square
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(2, 1));
         });
     });
 
